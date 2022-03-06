9fd5df9434ebe1b26ff911503f252fb2807b1c8f
Madalin <mabivolan@gmail.com>
2022-03-06 19:23:26 +0200

    Added search

---
+++ MapSearch/DataSource.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MapSearch
+{
+    class DataEntry
+    {
+        public string Judet;
+        public string Oras;
+        public string Firma;
+        public string NumeProprietar;
+        public string Tarla;
+        public string Parcela;
+        public string Suprafata;
+        public string DataContract;
+        public string StatusDosar;
+        public string LinkDocument;
+        public List<string> AdditionalDocument;
+
+        public int coord_x;
+        public int coord_y;
+
+        public DataEntry()
+        {
+            this.AdditionalDocument = new List<string>();
+        }
+
+        public List<String> toList()
+        {
+            return new List<string> {
+                this.Judet,
+                this.Oras,
+                this.Firma,
+                this.Tarla,
+                this.StatusDosar,
+                this.NumeProprietar,
+                this.DataContract,
+                this.Suprafata
+            };
+        }
+    }
+
+    class DataSource
+    {
+        //private string testPath = @"C:\Users\bivolan\source\repos\Test\DOLJ\ANM\BIRCA\config.json";
+
+        public List<List<String>> data;
+        public List<DataEntry> rawData;
+
+        public DataSource(List<String> paths)
+        {
+            this.data = new List<List<String>>();
+
+            List<string> files = findAllJson(new List<string> { @"C:\Users\bivolan\source\repos\Test\DOLJ", @"C:\Users\bivolan\source\repos\Test\OLT" });
+
+            foreach (string file in files)
+            {
+                convertJson(file);
+            }
+        }
+
+        private List<String> findAllJson(List<String> paths)
+        {
+            List<string> jsonFiles = new List<string>();
+
+            foreach (string path in paths)
+            {
+                jsonFiles.AddRange(System.IO.Directory.GetFiles(path, "*.json", SearchOption.AllDirectories).ToList());
+            }
+
+            return jsonFiles;
+        }
+        public void convertJson(string jsonPath)
+        {
+            string[] locatie = jsonPath.Split('\\');
+            string oras = locatie[locatie.Count() - 2];
+            string firma = locatie[locatie.Count() - 3];
+            string judet = locatie[locatie.Count() - 4];
+
+            string inputJson = File.ReadAllText(jsonPath);
+            List<DataEntry> newData = JsonConvert.DeserializeObject<List<DataEntry>>(inputJson);
+
+            foreach(DataEntry entry in rawData)
+            {
+                entry.Oras = oras;
+                entry.Judet = judet;
+                entry.Firma = firma;
+
+                this.data.Add(entry.toList());
+            }
+
+            this.rawData.AddRange(newData);
+        }
+    }
+}
---
+++ MapSearch/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ColumnCount = 8;
+
+            dataGridView1.Columns[0].Name = "Judet";
+            dataGridView1.Columns[1].Name = "Localitate";
+            dataGridView1.Columns[2].Name = "Firma";
+            dataGridView1.Columns[3].Name = "Tarla";
+            dataGridView1.Columns[4].Name = "Status";
+            dataGridView1.Columns[5].Name = "Nume";
+            dataGridView1.Columns[6].Name = "Data";
+            dataGridView1.Columns[7].Name = "Suprafata";
+
+            List<String> test = new List<string>();
+            test.Add("lol1");
+
+            DataSource newDataSource = new DataSource(new List<String>());
+
+            foreach (List<String> entry in newDataSource.data)
+            {
+                dataGridView1.Rows.Add(entry.ToArray());
+            }
+
+            foreach (List<String> entry in newDataSource.data)
+            {
+                // Faci o lista pentru oras 
+                // daca newDataSource.data[1] nu exista in lista, il bagi
+
+            }
+
+            // bagi lista in combobox
+
+        }
+    }
+}
